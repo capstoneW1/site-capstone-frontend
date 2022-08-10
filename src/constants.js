7f3ac5d9790cd598ecbb1ee7e12e72230e188490
Aligary <aligaryjr@gmail.com>
2022-08-10 15:12:23 -0700

    switched api base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:3001";
-//  const API_BASE_URL = "//elevate-sneakers.herokuapp.com";
+//const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "//elevate-sneakers.herokuapp.com";
 module.exports = {
   API_BASE_URL,
 };
